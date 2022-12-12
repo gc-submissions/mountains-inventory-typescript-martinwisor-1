ecc8d15aaea9d7a9eff1a4925b0330d9552c8312
martin <mawisor@gmail.com>
2022-12-12 14:22:04 -0500

    second commit

--- src/inventory.ts
+++ src/inventory.ts
@@ -1,26 +1,21 @@
 import { Product } from "./products";
 
 interface InventoryItem {
-  product?: Product;
-  name: string;
-  price: number;
+  product: Product;
   quantity: number;
 }
 
 let inventory: InventoryItem[] = [
   {
-    name: "motor",
-    price: 10.0,
+    product: { name: "motor", price: 10.0 },
     quantity: 10,
   },
   {
-    name: "sensor",
-    price: 12.5,
+    product: { name: "sensor", price: 12.5 },
     quantity: 4,
   },
   {
-    name: "LED",
-    price: 1.0,
+    product: { name: "LED", price: 1.0 },
     quantity: 20,
   },
 ];
@@ -28,8 +23,10 @@ let inventory: InventoryItem[] = [
 function calcInventoryValue(inventory: InventoryItem[]): number {
   let sum = 0;
   for (let i = 0; i < inventory.length; i++) {
-    sum += inventory[i].price * inventory[i].quantity;
+    sum += inventory[i].product.price * inventory[i].quantity;
   }
   return sum;
 }
 console.log(calcInventoryValue(inventory));
+
+export { calcInventoryValue, InventoryItem };
